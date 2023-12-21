4fc210822c48961b7d2b22131a78aa8c5ceae796
Jacob Phillips <jacob.p.phillips.5709@gmail.com>
2023-12-21 14:40:39 -0600

    Setup for Prisma ORM with initial migration

---
+++ prisma/migrations/20231221202014_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Movie" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Movie_pkey" PRIMARY KEY ("id")
+);
