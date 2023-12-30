b834bd63f6f832fa5e550af62e198b475b0d77a8
Jacob Phillips <jacob.p.phillips.5709@gmail.com>
2023-12-30 11:54:09 -0600

    NavSidebar Link simplifications with functional nav-buttons

--- src/components/NavSidebar.tsx
+++ src/components/NavSidebar.tsx
@@ -11,22 +11,24 @@ export default function NavSidebar() {
 
 	return (
 		<nav className={`bg-gradient-to-b from-blue-900 to-fuchsia-950 h-screen p-5 pt-8 relative transform ease-in-out origin-left ${open ? "w-72" : "w-20"} duration-1000`}>
+
 			<BsArrowLeftShort
 				className={`bg-white text-blue-900 text-3xl border border-indigo-800 rounded-full absolute -right-4 top-9 transform ease-in-out origin-center ${!open && "rotate-180"} duration-1000 hover:cursor-pointer`}
 				onClick={() => setOpen(!open)}
 			/>
-			<div className="inline-flex">
-				<PiDevToLogo className="bg-amber-300 text-4xl rounded block float-left mr-2" />
-				<Link
-					href="/"
-					className={`text-white font-medium text-2xl transform ease-in-out origin-left ${!open && "scale-0"} duration-1000`}
-				>
-					Logo
-				</Link>
-			</div>
+
+			<Link href="/">
+				<div className="inline-flex">
+					<PiDevToLogo className="bg-amber-300 text-4xl rounded block float-left mr-2 hover:cursor-pointer" />
+					<span className={`text-white font-medium text-2xl transform ease-in-out origin-left ${!open && "scale-0"} duration-1000`}>
+						Logo
+					</span>
+				</div>
+			</Link>
+
 			<div className={`flex items-center rounded-md bg-white/[.18] mt-6 py-2 ${open ? "px-4" : "px-2"} duration-1000`}>
 				<span className={`text-white text-lg block float-left transform ease-in-out origin-right ${open ? "mr-2" : "pl-0.5"} duration-1000`}>
-					<BsSearch />
+					<BsSearch className="hover:cursor-pointer" />
 				</span>
 				<input
 					type="search"
@@ -34,29 +36,30 @@ export default function NavSidebar() {
 					className={`text-base bg-transparent text-white full transform ease-in-out origin-left ${!open && "scale-0"} duration-1000 focus:outline-none`}
 				/>
 			</div>
+
 			<div className="pt-2">
-				<div className="text-gray-300 text-sm flex items-center gap-x-4 mt-2 p-2 rounded-md hover:bg-white/[.18]">
+				<Link
+					href="/"
+					className="text-gray-300 text-sm flex items-center gap-x-4 mt-2 p-2 rounded-md hover:bg-white/[.18]"
+				>
 					<span className="text-2xl block float-left">
 						<FaHome />
 					</span>
-					<Link
-						href="/"
-						className={`text-base font-medium transform ease-in-out origin-left ${!open && "scale-0"} duration-1000`}
-					>
+					<span className={`text-base font-medium transform ease-in-out origin-left ${!open && "scale-0"} duration-1000`}>
 						Home
-					</Link>
-				</div>
-				<div className="text-gray-300 text-sm flex items-center gap-x-4 mt-2 p-2 rounded-md hover:bg-white/[.18]">
+					</span>
+				</Link>
+				<Link
+					href="/movies"
+					className="text-gray-300 text-sm flex items-center gap-x-4 mt-2 p-2 rounded-md hover:bg-white/[.18]"
+				>
 					<span className="text-2xl block float-left">
 						<MdLocalMovies />
 					</span>
-					<Link
-						href="/movies"
-						className={`text-base font-medium transform ease-in-out origin-left ${!open && "scale-0"} duration-1000`}
-					>
+					<span className={`text-base font-medium transform ease-in-out origin-left ${!open && "scale-0"} duration-1000`}>
 						Movies
-					</Link>
-				</div>
+					</span>
+				</Link>
 			</div>
 		</nav>
 	)
