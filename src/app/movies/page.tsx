5a4df0d8b867fb229736c643d40d2262ec4cb6e7
Jacob Phillips <jacob.p.phillips.5709@gmail.com>
2023-12-31 11:41:34 -0600

    Simplifies the table column widths (_spec the header, not the repeater_)

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -15,17 +15,17 @@ export default async function Movies() {
 				<table className="min-w-full border-b border-black mb-2">
 					<thead className="border-b border-black">
 						<tr>
-							<th className="text-left p-1">Id</th>
-							<th className="text-left p-1">Title</th>
-							<th className="text-left p-1">Actions</th>
+							<th className="text-left w-[10%] p-1">Id</th>
+							<th className="text-left w-[45%] p-1">Title</th>
+							<th className="text-left w-[45%] p-1">Actions</th>
 						</tr>
 					</thead>
 					<tbody>
 						{movies.map(movie => (
 							<tr key={movie.id} className="odd:bg-gray-50 hover:bg-gray-100">
-								<td className="text-left w-[10%] p-1">...{movie.id.substring(movie.id.length - 6, movie.id.length)}</td>
-								<td className="text-left w-[45%] p-1">{movie.title}</td>
-								<td className="text-left w-[45%] p-1">
+								<td className="text-left p-1">...{movie.id.substring(movie.id.length - 6, movie.id.length)}</td>
+								<td className="text-left p-1">{movie.title}</td>
+								<td className="text-left p-1">
 									<button
 										type="button"
 										className="bg-transparent text-blue-500 border border-blue-500 px-3 py-1.5 m-1 rounded hover:bg-blue-500 hover:text-white hover:border-transparent"
