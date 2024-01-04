ce18a4edc676d3575f03dece91c3b3b07beae9ba
Jacob Phillips <jacob.p.phillips.5709@gmail.com>
2024-01-04 13:31:08 -0600

    Adds spinner indicators to the Movie Table

--- src/components/MovieTable.tsx
+++ src/components/MovieTable.tsx
@@ -3,6 +3,7 @@ import { removeMovie } from "@/app/movies/actions"
 import { MovieResponse, getMovies } from "@/app/movies/fetchers"
 import Link from "next/link"
 import { useEffect, useState } from "react"
+import { ImSpinner9 } from "react-icons/im";
 
 export default function MovieTable() {
 	const [movies, setMovies] = useState<MovieResponse[]>([])
@@ -26,46 +27,63 @@ export default function MovieTable() {
 				</tr>
 			</thead>
 			<tbody>
-				{isLoading ? "Loading..." : movies.map(movie => (
-					<tr key={movie.id} className="odd:bg-gray-50 hover:bg-gray-100">
-						<td className="text-left p-1">...{movie.id.substring(movie.id.length - 6, movie.id.length)}</td>
-						<td className="text-left p-1">{movie.title}</td>
+				{isLoading ?
+					<tr>
 						<td className="text-left p-1">
-							<div className="flex items-center gap-x-1">
-								<Link href={`/movies/${movie.id}`}>
-									<button
-										type="button"
-										className="bg-transparent text-blue-500 border border-blue-500 px-3 py-1.5 m-1 rounded hover:bg-blue-500 hover:text-white hover:border-transparent"
-									>
-										Update
-									</button>
-								</Link>
-								<form action={removeMovie}>
-									<button
-										type="submit"
-										name="movieId"
-										className="bg-red-700 text-white border border-red-700 px-3 py-1.5 m-1 rounded hover:bg-red-900 hover:border-transparent"
-										value={movie.id}
-									>
-										Delete
-									</button>
-								</form>
-							</div>
+							<ImSpinner9 className="text-2xl text-blue-500 m-4 rounded-full animate-[spin_1.5s_linear_infinite]" />
+						</td>
+						<td className="text-left p-1">
+							<ImSpinner9 className="text-2xl text-blue-500 m-4 rounded-full animate-[spin_1.5s_linear_infinite]" />
+						</td>
+						<td className="text-left p-1">
+							<ImSpinner9 className="text-2xl text-blue-500 m-4 rounded-full animate-[spin_1.5s_linear_infinite]" />
 						</td>
 					</tr>
-				))}
+					:
+					movies.map(movie => (
+						<tr key={movie.id} className="odd:bg-gray-50 hover:bg-gray-100">
+							<td className="text-left p-1">...{movie.id.substring(movie.id.length - 6, movie.id.length)}</td>
+							<td className="text-left p-1">{movie.title}</td>
+							<td className="text-left p-1">
+								<div className="flex items-center gap-x-1">
+									<Link href={`/movies/${movie.id}`}>
+										<button
+											type="button"
+											className="bg-transparent text-blue-500 border border-blue-500 px-3 py-1.5 m-1 rounded hover:bg-blue-500 hover:text-white hover:border-transparent"
+										>
+											Update
+										</button>
+									</Link>
+									<form action={removeMovie}>
+										<button
+											type="submit"
+											name="movieId"
+											className="bg-red-700 text-white border border-red-700 px-3 py-1.5 m-1 rounded hover:bg-red-900 hover:border-transparent"
+											value={movie.id}
+										>
+											Delete
+										</button>
+									</form>
+								</div>
+							</td>
+						</tr>
+					))}
 				<tr className="border-t border-black">
 					<td />
 					<td />
 					<td className="text-left p-1">
-						<Link href="/movies/create">
-							<button
-								type="button"
-								className="bg-transparent text-green-700 border border-green-700 px-3 py-1.5 m-1 rounded hover:bg-green-700 hover:text-white hover:border-transparent"
-							>
-								Create
-							</button>
-						</Link>
+						{isLoading ?
+							<ImSpinner9 className="text-2xl text-blue-500 m-4 rounded-full animate-[spin_1.5s_linear_infinite]" />
+							:
+							<Link href="/movies/create">
+								<button
+									type="button"
+									className="bg-transparent text-green-700 border border-green-700 px-3 py-1.5 m-1 rounded hover:bg-green-700 hover:text-white hover:border-transparent"
+								>
+									Create
+								</button>
+							</Link>
+						}
 					</td>
 				</tr>
 			</tbody>
