b98b6efd24053357db6de1d0dbf40da338c9c17d
Jacob Phillips <jacob.p.phillips.5709@gmail.com>
2024-01-10 11:30:36 -0600

    Update the demo paragraph

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -15,7 +15,7 @@ export default async function Movies() {
 		<main className="w-full">
 			<article className="p-8">
 				<h1 className="text-4xl font-medium mb-2">Movie Collection</h1>
-				<p className="mb-4">TODO: This "thingy" demonstrates "what concepts"...?</p>
+				<p className="mb-4">This page hierarchy demonstrates the relationship between server-components, server-actions and client-components</p>
 				<table className="min-w-full">
 					<thead className="border-b border-black">
 						<tr>
